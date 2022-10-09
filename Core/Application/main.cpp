b261f3fd4834e138d056d33a5af0b22821e30521
Milos Vasic <i@mvasic.ru>
2022-10-09 16:05:13 +0300

    Program arguments - The first touches.

--- Core/Application/main.cpp
+++ Core/Application/main.cpp
@@ -8,7 +8,7 @@
 #include "BuildConfig.h"
 #include "VersionInfo.h"
 
-#include "generated/Source/cpp/Project.h"
+#include "generated/Source/cpp/Label.h"
 
 using namespace Utils;
 using namespace Commons::IO;
@@ -16,10 +16,61 @@ using namespace Commons::Strings;
 
 int main(int argc, char *argv[]) {
 
-    Project project;
-    project.setTitle("Hello");
-    project.setIdentifier("World");
+    Label label;
+    label.setTitle("Generated code");
+    label.setDescription("READY");
 
-    v(project.getTitle(), project.getIdentifier());
+    auto errTag = "error";
+    auto paramsTag = "parameters";
+
+    argparse::ArgumentParser program(VERSIONABLE_NAME, getVersion());
+
+    program.add_argument("-l", "--logFull")
+            .default_value(false)
+            .implicit_value(true)
+            .help("Log with the full details");
+
+    program.add_argument("-d", "--debug")
+            .default_value(false)
+            .implicit_value(true)
+            .help("Additional information related to the parsing and code generating");
+
+    std::string epilog("Project homepage: ");
+    epilog.append(getHomepage());
+
+    program.add_description(getDescription());
+    program.add_epilog(epilog);
+
+    try {
+
+        program.parse_args(argc, argv);
+
+    } catch (const std::runtime_error &err) {
+
+        e(errTag, err.what());
+        std::exit(1);
+    }
+
+    try {
+
+        setLogFull(program["--logFull"] == true);
+        setDebug(program["--debug"] == true && logFull());
+
+        if (logFull()) {
+
+            v(paramsTag, "Full-log mode is on");
+        }
+
+        if (isDebug()) {
+
+            w(paramsTag, "Debug mode is on");
+            v(label.getTitle(), label.getDescription());
+        }
+
+    } catch (std::logic_error &err) {
+
+        e(errTag, err.what());
+        std::exit(1);
+    }
     return 0;
 }
