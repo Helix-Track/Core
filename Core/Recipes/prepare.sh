d82f3b012ab253e4659d6ae6ec79e6e4cb215092
Milos Vasic <i@mvasic.ru>
2022-10-09 16:25:16 +0300

    The 'prepare' installation recipe fix.

--- Core/Recipes/prepare.sh
+++ Core/Recipes/prepare.sh
@@ -8,26 +8,25 @@ if test -e "$GENERATED"; then
 
     echo "ERROR: Could not remove the '$GENERATED' directory"
     exit 1
-  else
-
-    echo "Generating the code"
-
-    if mkdir "$GENERATED" && /usr/local/bin/sql2code-0.0.1/sql2code -i \
-                Database/DDL/Definition.V1.sql \
-                Database/DDL/Extensions/Chats/Definition.V1.sql \
-                Database/DDL/Extensions/Documents/Definition.V1.sql \
-                Database/DDL/Extensions/Times/Definition.V1.sql \
-                -t cpp -o "$GENERATED"; then
-
-        echo "Code generated"
-
-    else
-
-        echo "ERROR: Code not generated"
-
-        exit 1
-    fi
   fi
+fi
+
+echo "Generating the code"
+
+if mkdir "$GENERATED" && /usr/local/bin/sql2code-0.0.1/sql2code -i \
+            Database/DDL/Definition.V1.sql \
+            Database/DDL/Extensions/Chats/Definition.V1.sql \
+            Database/DDL/Extensions/Documents/Definition.V1.sql \
+            Database/DDL/Extensions/Times/Definition.V1.sql \
+            -t cpp -o "$GENERATED"; then
+
+    echo "Code generated"
+
+else
+
+    echo "ERROR: Code not generated"
+
+    exit 1
 fi
 
 echo "Preparing the 'HelixTrack Core' for the installation" && \
