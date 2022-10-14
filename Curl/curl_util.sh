755935aec996de1e6abd455157f7b801ff8cbc5e
Milos Vasic <i@mvasic.ru>
2022-10-14 16:04:19 +0300

    Curl - Util.

---
+++ Curl/curl_util.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# TODO: Move into the commonly used utility!
+function RUN_CURL {
+
+  CURL_UTIL_PORT="80"
+  CURL_UTIL_ENDPOINT=""
+  CURL_UTIL_HOST="127.0.0.1"
+  CURL_UTIL_PROTOCOL="https"
+
+  if [ -n "$1" ]; then
+
+    CURL_UTIL_PROTOCOL="$1"
+  fi
+
+  if [ -n "$2" ]; then
+
+    CURL_UTIL_HOST="$2"
+  fi
+
+  if [ -n "$3" ]; then
+
+    CURL_UTIL_PORT="$3"
+  fi
+
+  if [ -n "$4" ]; then
+
+    CURL_UTIL_ENDPOINT="$4"
+  fi
+
+  TARGET="$CURL_UTIL_PROTOCOL"://"$CURL_UTIL_HOST":"$CURL_UTIL_PORT"/"$CURL_UTIL_ENDPOINT"
+  clear && echo "URL: $TARGET" && curl -v "$TARGET" && echo ""
+}
--- Curl/version.sh
+++ Curl/version.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-echo "ERROR: Not implemented"
-exit 1
+CURL="curl_util.sh"
+
+if test -e "$CURL"; then
+
+  . curl_util.sh
+
+else
+
+  echo "ERROR: '$CURL' not found"
+  exit 1
+fi
+
+RUN_CURL "$1" "$2" "$3" "$4"
