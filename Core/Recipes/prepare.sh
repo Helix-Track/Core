a20b7ccc25485a15995712ef6b2aef96dc1b6a1b
Milos Vasic <i@mvasic.ru>
2022-10-24 17:19:49 +0300

    Extending the 'prepare' recipe.

--- Core/Recipes/prepare.sh
+++ Core/Recipes/prepare.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+HERE="$(pwd)"
+PLUGINS="Application/plugins"
 GENERATED="Application/generated"
 
 if test -e "$GENERATED"; then
@@ -27,6 +29,42 @@ else
     echo "ERROR: Code not generated"
 
     exit 1
+fi
+
+if test -e "$PLUGINS"; then
+
+  if ! rm -rf "$PLUGINS"; then
+
+    echo "ERROR: Could not remove '$PLUGINS'"
+    exit 1
+  fi
+fi
+
+if ! mkdir -p "$PLUGINS"; then
+
+  echo "ERROR: Could not create '$PLUGINS'"
+  exit 1
+fi
+
+if [ -z "$DEPENDABLE_DEPENDENCIES_HOME" ]; then
+
+  DEPENDABLE_DEPENDENCIES_HOME="$HERE"
+fi
+
+echo "The dependencies home directory: '$DEPENDABLE_DEPENDENCIES_HOME'"
+
+DEPENDENCIES_WORKING_DIRECTORY="$DEPENDABLE_DEPENDENCIES_HOME/_Dependencies"
+
+echo "The dependencies working directory: '$DEPENDENCIES_WORKING_DIRECTORY'"
+
+if cp "$DEPENDENCIES_WORKING_DIRECTORY/Cache/JWT-Drogon/Library/JWT.*" "$PLUGINS" && \
+  cp "$DEPENDENCIES_WORKING_DIRECTORY/Cache/JWT-Drogon/Library/JWT*.*" "$PLUGINS"; then
+
+  echo "Drogon JWT plugin copied"
+else
+
+  echo "ERROR: Drogon JWT plugin copied not copied"
+  exit 1
 fi
 
 echo "Preparing the 'HelixTrack Core' for the installation" && \
