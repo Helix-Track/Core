a2dd512a1c8dabb346b73ffc0fbaea5b47eaab88
Milos Vasic <i@mvasic.ru>
2022-11-04 14:29:21 +0300

    The JWT token.

--- Core/Database/DDL/Services/Authentication/Definition.V1.sql
+++ Core/Database/DDL/Services/Authentication/Definition.V1.sql
@@ -36,6 +36,7 @@ CREATE TABLE user
     id       TEXT    NOT NULL PRIMARY KEY UNIQUE,
     username TEXT    NOT NULL UNIQUE,
     password TEXT    NOT NULL,
+    token    TEXT,
     created  INTEGER NOT NULL,
     modified INTEGER NOT NULL,
     deleted  BOOLEAN NOT NULL CHECK (deleted IN (0, 1))
