4f348f6ae604cc2c7c060881af5168f2e9a8ed40
Milos Vasic <i@mvasic.ru>
2023-08-10 17:16:12 +0300

    Cleaning up.

--- Core/Dependencies/Logger.sh
+++ Core/Dependencies/Logger.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 export DEPENDABLE_REPOSITORY="git@github.com:red-elf/Logger.git"
-export DEPENDABLE_BRANCH="main"
--- Core/Dependencies/Tester.sh
+++ Core/Dependencies/Tester.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 export DEPENDABLE_REPOSITORY="git@github.com:red-elf/Tester.git"
-export DEPENDABLE_BRANCH="main"
