695d296931d46f225d5ae382581f043356b41eb3
Milos Vasic <i@mvasic.ru>
2023-08-17 14:40:40 +0300

    Hook recipe implementation.

--- Recipes/push_all_hook.sh
+++ Recipes/push_all_hook.sh
@@ -5,4 +5,20 @@ if [ -n "$1" ]; then
     echo "Executing post push steps for upstreams from $1"
 fi
 
-echo "KK"
+if [ -z "$SUBMODULES_HOME" ]; then
+
+  echo "ERROR: The SUBMODULES_HOME is not defined"
+  exit 1
+fi
+
+SCRIPT_GATHER_SUBMODULES="$SUBMODULES_HOME/Software-Toolkit/Utils/Git/gather_submodules.sh"
+
+if test -e "$SCRIPT_GATHER_SUBMODULES"; then
+
+  sh "$SCRIPT_GATHER_SUBMODULES"
+
+else
+
+  echo "ERROR: Script not found '$SCRIPT_GATHER_SUBMODULES'"
+  exit 1
+fi
