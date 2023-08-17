49ebe2ca65e1337e31f5e0fdb007ddc3e52b441a
Milos Vasic <i@mvasic.ru>
2023-08-17 16:57:14 +0300

    Flags support polishing.

--- Recipes/push_all_hook.sh
+++ Recipes/push_all_hook.sh
@@ -1,24 +1,54 @@
 #!/bin/bash
 
-if [ -n "$1" ]; then
+# TODO: Re-enable hook and complete the implementation
 
-    echo "Executing post push steps for upstreams from $1"
-fi
+exit 0
 
-if [ -z "$SUBMODULES_HOME" ]; then
+# if [ -z "$1" ]; then
 
-  echo "ERROR: The SUBMODULES_HOME is not defined"
-  exit 1
-fi
+#     echo "ERROR: Upstreams directory path is mandatory"
+# fi
 
-SCRIPT_GATHER_SUBMODULES="$SUBMODULES_HOME/Software-Toolkit/Utils/Git/gather_submodules.sh"
+# DIR_UPSTREAMS="$1"
+# DIR_ROOT="$DIR_UPSTREAMS/.."
+# DIR_GIT="$DIR_ROOT/.git"
 
-if test -e "$SCRIPT_GATHER_SUBMODULES"; then
+# if ! test -e "$DIR_GIT"; then
 
-  sh "$SCRIPT_GATHER_SUBMODULES" "FLAGS=FLAG_UPDATE_ALWAYS;FLAG_HELLO"
+#     echo "ERROR: Not a Git root directory '$DIR_GIT'"
+#     exit 1
+# fi
 
-else
+# if [ -z "$SUBMODULES_HOME" ]; then
 
-  echo "ERROR: Script not found '$SCRIPT_GATHER_SUBMODULES'"
-  exit 1
-fi
+#   echo "ERROR: The SUBMODULES_HOME is not defined"
+#   exit 1
+# fi
+
+# SCRIPT_FLAGS="$SUBMODULES_HOME/Software-Toolkit/Utils/Git/gather_submodules_flags.sh"
+# SCRIPT_GATHER_SUBMODULES="$SUBMODULES_HOME/Software-Toolkit/Utils/Git/gather_submodules.sh"
+
+# if test -e "$SCRIPT_GATHER_SUBMODULES"; then
+
+#     if test -e "$SCRIPT_FLAGS"; then
+
+#         # shellcheck disable=SC1090
+#         . "$SCRIPT_FLAGS"
+
+#     else
+
+#         echo "ERROR: Flags Script not found '$SCRIPT_FLAGS'"
+#         exit 1
+#     fi
+
+#     F_UPDATE_ONLY="${OPEN}${FLAG_UPDATE_ONLY}=${DIR_ROOT}${CLOSE}"
+    
+#     FLAGS="FLAGS=[${F_UPDATE_ONLY}]"
+
+#     sh "$SCRIPT_GATHER_SUBMODULES" "$FLAGS"
+
+# else
+
+#   echo "ERROR: Script not found '$SCRIPT_GATHER_SUBMODULES'"
+#   exit 1
+# fi
