8f45a41cea5839563ad8a3bc8c62ea9b71fdc357
Milos Vasic <i@mvasic.ru>
2023-08-20 16:08:48 +0300

    Support for the flag completed.

--- Recipes/push_all_hook.sh
+++ Recipes/push_all_hook.sh
@@ -1,54 +1,50 @@
 #!/bin/bash
 
-# TODO: Re-enable hook and complete the implementation
+if [ -z "$1" ]; then
 
-exit 0
+    echo "ERROR: Upstreams directory path is mandatory"
+fi
 
-# if [ -z "$1" ]; then
+DIR_UPSTREAMS="$1"
+DIR_ROOT="$DIR_UPSTREAMS/.."
+DIR_GIT="$DIR_ROOT/.git"
 
-#     echo "ERROR: Upstreams directory path is mandatory"
-# fi
+if ! test -e "$DIR_GIT"; then
 
-# DIR_UPSTREAMS="$1"
-# DIR_ROOT="$DIR_UPSTREAMS/.."
-# DIR_GIT="$DIR_ROOT/.git"
+    echo "ERROR: Not a Git root directory '$DIR_GIT'"
+    exit 1
+fi
 
-# if ! test -e "$DIR_GIT"; then
+if [ -z "$SUBMODULES_HOME" ]; then
 
-#     echo "ERROR: Not a Git root directory '$DIR_GIT'"
-#     exit 1
-# fi
+  echo "ERROR: The SUBMODULES_HOME is not defined"
+  exit 1
+fi
 
-# if [ -z "$SUBMODULES_HOME" ]; then
+SCRIPT_FLAGS="$SUBMODULES_HOME/Software-Toolkit/Utils/Git/gather_submodules_flags.sh"
+SCRIPT_GATHER_SUBMODULES="$SUBMODULES_HOME/Software-Toolkit/Utils/Git/gather_submodules.sh"
 
-#   echo "ERROR: The SUBMODULES_HOME is not defined"
-#   exit 1
-# fi
+if test -e "$SCRIPT_GATHER_SUBMODULES"; then
 
-# SCRIPT_FLAGS="$SUBMODULES_HOME/Software-Toolkit/Utils/Git/gather_submodules_flags.sh"
-# SCRIPT_GATHER_SUBMODULES="$SUBMODULES_HOME/Software-Toolkit/Utils/Git/gather_submodules.sh"
+    if test -e "$SCRIPT_FLAGS"; then
 
-# if test -e "$SCRIPT_GATHER_SUBMODULES"; then
+        # shellcheck disable=SC1090
+        . "$SCRIPT_FLAGS"
 
-#     if test -e "$SCRIPT_FLAGS"; then
+    else
 
-#         # shellcheck disable=SC1090
-#         . "$SCRIPT_FLAGS"
+        echo "ERROR: Flags Script not found '$SCRIPT_FLAGS'"
+        exit 1
+    fi
 
-#     else
-
-#         echo "ERROR: Flags Script not found '$SCRIPT_FLAGS'"
-#         exit 1
-#     fi
-
-#     F_UPDATE_ONLY="${OPEN}${FLAG_UPDATE_ONLY}=${DIR_ROOT}${CLOSE}"
+    F_UPDATE_ONLY="${OPEN}${FLAG_UPDATE_ONLY}=${DIR_ROOT}${CLOSE}"
     
-#     FLAGS="FLAGS=[${F_UPDATE_ONLY}]"
+    FLAGS="FLAGS=[${F_UPDATE_ONLY}]"
 
-#     sh "$SCRIPT_GATHER_SUBMODULES" "$FLAGS"
+    sh "$SCRIPT_GATHER_SUBMODULES" "$FLAGS"
 
-# else
+else
 
-#   echo "ERROR: Script not found '$SCRIPT_GATHER_SUBMODULES'"
-#   exit 1
-# fi
+  echo "ERROR: Script not found '$SCRIPT_GATHER_SUBMODULES'"
+  exit 1
+fi
