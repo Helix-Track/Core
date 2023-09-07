bab0ebba80abe3d471166a703cd299e1ca47b862
Milos Vasic <i@mvasic.ru>
2023-09-07 13:19:29 +0300

    Adding linking support.

--- Recipes/project_pre_open.sh
+++ Recipes/project_pre_open.sh
@@ -22,5 +22,43 @@ EXECUTE_RECIPE() {
     fi
 }
 
+LINK_MODULE() {
+
+    if [ -z "$1" ]; then
+
+        ecgo "ERROR: Module source parameter is mandatory"
+        exit 1
+    fi
+
+    if [ -z "$2" ]; then
+
+        ecgo "ERROR: Module destination parameter is mandatory"
+        exit 1
+    fi
+
+    PREFIX=""
+
+    if [ -n "$3" ]; then
+
+        PREFIX="$3"
+    fi
+
+    HERE="$(pwd)"
+    MODULE_SOURCE="$1"
+    MODULE_DESTINATION="${PREFIX}$2"
+
+    DIR_SOURCE="$HERE/$MODULE_SOURCE"
+    DIR_DESTINATION="$HERE/$MODULE_DESTINATION"
+
+    echo "Linking: $DIR_SOURCE -> $DIR_DESTINATION"
+
+    # TODO: Implement linking
+}
+
+PREFIX="module_"
+
+LINK_MODULE "Upstreamable" "Upstreamable" "$PREFIX"
+LINK_MODULE "Software-Toolkit" "Toolkit" "$PREFIX"
+
 EXECUTE_RECIPE "$SCRIPT_CORE_RECIPE_PRE_OPEN"
 EXECUTE_RECIPE "$SCRIPT_PROPRIATERY_RECIPE_PRE_OPEN"
