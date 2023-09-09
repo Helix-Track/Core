ca7731ffb9ab65d955b362eb7b0f0ae071ed9d88
Milos Vasic <i@mvasic.ru>
2023-09-09 18:17:12 +0300

    Cleaning up.

--- Run/Update/toolkit_Core.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-HERE="$(pwd)"
-
-cd "$HERE/Core" && sh Toolkit/update_software_toolkit.sh
--- Run/Update/toolkit_Propriatery.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-HERE="$(pwd)"
-
-cd "$HERE/Propriatery" && sh Toolkit/update_software_toolkit.sh
