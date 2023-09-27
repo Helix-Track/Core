06db4846339d80a5273f9ebb3b28474bbfce6b56
Milos Vasic <i@mvasic.ru>
2023-09-27 17:58:23 +0300

    Run shared local.

---
+++ Run/Httpd/shared_Local.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$SUBMODULES_HOME" ]; then
+
+  echo "ERROR: SUBMODULES_HOME not available"
+  exit 1
+fi
+
+SCRIPT_GET_HTTPD="$SUBMODULES_HOME/Software-Toolkit/Utils/Httpd/get_httpd"
+
+if ! test -e "$SCRIPT_GET_HTTPD"; then
+
+    echo "ERROR: Script not found '$SCRIPT_GET_HTTPD'"
+    exit 1
+fi
+
+if [ -z HTTPD_WEB_ROOT_SHARED ]; then
+
+    echo "ERROR: Env. variable not defined 'HTTPD_WEB_ROOT_SHARED'"
+    exit 1
+fi
+
+sh "$SCRIPT_GET_HTTPD" "httpd.Shared" "$HTTPD_WEB_ROOT_SHARED" 8081
