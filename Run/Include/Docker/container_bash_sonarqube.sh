8cb74dfb921eb99f424932075ebc680186f7b7eb
Milos Vasic <i@mvasic.ru>
2023-09-28 13:55:17 +0300

    Run configurations paths fixed.

--- Run/Include/Docker/container_bash_sonarqube.sh
+++ Run/Include/Docker/container_bash_sonarqube.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ -z "$SUBMODULES_HOME" ]; then
+
+  echo "ERROR: The SUBMODULES_HOME is not defined"
+  exit 1
+fi
+
 if [ -n "$1" ]; then
 
   HERE="$(pwd)"
 
-  cd "$HERE" && sh Core/Toolkit/Utils/Docker/get_container_terminal.sh "sonarqube.$1"
+  cd "$HERE" && sh "$SUBMODULES_HOME"/Software-Toolkit/Utils/Docker/get_container_terminal.sh "sonarqube.$1"
 
 else
 
