5b2562619d04b545aadfb56df2f62a8c2bbc577b
Milos Vasic <i@mvasic.ru>
2023-10-02 14:43:52 +0300

    Run configuration added.

---
+++ Run/Install/sonar_Scanner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$SUBMODULES_HOME" ]; then
+
+  echo "ERROR: The SUBMODULES_HOME is not defined"
+  exit 1
+fi
+
+DIR_TOOLKIT="$SUBMODULES_HOME/Software-Toolkit"
+
+sh "$DIR_TOOLKIT/Utils/SonarQube/get_sonar_scanner.sh"
