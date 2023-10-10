2644dff85ea7101f7886a9d58dc1f2cab60c4dde
Milos Vasic <i@mvasic.ru>
2023-10-10 14:46:28 +0300

    Extending the setings JSON - First touches.

--- Run/Install/settings_JSON.sh
+++ Run/Install/settings_JSON.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+HERE=$(pwd)
+
 if [ -z "$SUBMODULES_HOME" ]; then
 
   echo "ERROR: The SUBMODULES_HOME is not defined"
@@ -8,4 +10,4 @@ fi
 
 DIR_TOOLKIT="$SUBMODULES_HOME/Software-Toolkit"
 
-sh "$DIR_TOOLKIT/Utils/SonarQube/configure_sonar_lint.sh"
+sh "$DIR_TOOLKIT/Utils/VSCode/extend_settings_json.sh" "$HERE/Recipes/settings.json.sh"
