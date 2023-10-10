d376a0be806c3c831ed0db33022635e007b51dc6
Milos Vasic <i@mvasic.ru>
2023-10-10 18:39:25 +0300

    Initialize VSCode settings.

--- Recipes/vscode_installation_parameters.sh
+++ Recipes/vscode_installation_parameters.sh
@@ -9,7 +9,7 @@ if [ -z "$SHARES_SERVER" ]; then
 fi
 
 # shellcheck disable=SC2034
-DATA_VERSION="1.0.6"
+DATA_VERSION="1.0.7"
 
 # shellcheck disable=SC2034
 DIR_INSTALLATION_HOME="$DIR_HOME/Workspaces/VSCode/Linux/VSCode"
