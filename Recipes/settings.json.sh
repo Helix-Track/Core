35a94bdcdb4d69441eed62078897c26ca2d68173
Milos Vasic <i@mvasic.ru>
2023-10-10 14:13:05 +0300

    Get SonarLint settings - First touches.

--- Recipes/settings.json.sh
+++ Recipes/settings.json.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
 cat << EOF
-"sonarlint.connectedMode.connections.sonarqube": [
-    {
-        "serverUrl": "$SONARQUBE_SERVER",
-        "connectionId": "$SONARQUBE_PROJECT",
-        "token": "$SONARQUBE_TOKEN"
-    }
-],
-"sonarlint.connectedMode.project": {
-    "connectionId": "$SONARQUBE_PROJECT",
-    "projectKey": "$SONARQUBE_PROJECT",
-}
-"sonarlint.analyzerProperties": {
-
+{
+    "markdown-pdf.convertOnSave": true
 }
 EOF
