592d4280bd473178522eb81f660d62344d65381f
Milos Vasic <i@mvasic.ru>
2023-10-10 16:07:19 +0300

    Mergint the JSON content(s).

--- Run/Install/settings_JSON.sh
+++ Run/Install/settings_JSON.sh
@@ -9,5 +9,75 @@ if [ -z "$SUBMODULES_HOME" ]; then
 fi
 
 DIR_TOOLKIT="$SUBMODULES_HOME/Software-Toolkit"
+RECIPE_SETTINGS_JSON="$HERE/Recipes/settings.json.sh"
+SCRIPT_GET_PROGRAM="$DIR_TOOLKIT/Utils/Sys/Programs/get_program.sh"
+SCRIPT_EXTEND_JSON="$DIR_TOOLKIT/Utils/Sys/JSON/merge_jsons.sh"
 
-sh "$DIR_TOOLKIT/Utils/VSCode/extend_settings_json.sh" "$HERE/Recipes/settings.json.sh"
+if ! test -e "$SCRIPT_GET_PROGRAM"; then
+
+  echo "ERROR: Script not found '$SCRIPT_GET_PROGRAM'"
+  exit 1
+fi
+
+if ! test -e "$SCRIPT_EXTEND_JSON"; then
+
+  echo "ERROR: Script not found '$SCRIPT_EXTEND_JSON'"
+  exit 1
+fi
+
+if sh "$SCRIPT_GET_PROGRAM" code >/dev/null 2>&1; then
+
+  CODE_PATH=$(which code)
+
+  if test -e "$CODE_PATH"; then
+
+    echo "VSCode path: '$CODE_PATH'"
+
+  else
+
+    echo "ERROR: VSCode Path not found '$CODE_PATH'"
+    exit 1
+  fi
+
+  CODE_DIR=$(dirname "$CODE_PATH")
+  SETTINGS_JSON="$CODE_DIR/data/user-data/User/settings.json"
+
+  echo "Checking: '$SETTINGS_JSON'"
+
+  if test -e "$SETTINGS_JSON"; then
+
+    echo "Settings JSON: '$SETTINGS_JSON'"
+
+  else
+
+    if echo "{}" >> "$SETTINGS_JSON"; then
+
+      echo "Settings JSON created: '$SETTINGS_JSON'"
+
+    else
+
+      echo "ERROR: Could not create '$SETTINGS_JSON'"
+      exit 1
+    fi
+  fi
+
+  if test -e "$RECIPE_SETTINGS_JSON"; then
+
+    echo "Settings JSON recipe: '$RECIPE_SETTINGS_JSON'"
+
+  else
+
+    echo "ERROR: Settings JSON recipe not found '$RECIPE_SETTINGS_JSON'"
+    exit 1
+  fi
+
+  if sh "$SCRIPT_EXTEND_JSON" "$SETTINGS_JSON" "$RECIPE_SETTINGS_JSON" "$SETTINGS_JSON"; then
+
+    echo "Settings JSON has been extended with success"
+
+  else
+
+    echo "ERROR: Settings JSON failed to extend"
+    exit 1
+  fi
+fi
