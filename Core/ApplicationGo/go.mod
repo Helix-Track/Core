b5d246c474e4426acdc4ddacb2311b74833dd5a9
Milos Vasic <i@mvasic.ru>
2023-10-22 21:29:35 +0300

    Playground - Go - Print args.

--- Core/ApplicationGo/go.mod
+++ Core/ApplicationGo/go.mod
@@ -1,3 +1,3 @@
-module example/hello
+module exercise/sonarQube
 
-go 1.18
+go 1.20
--- Core/ApplicationGo/hello.go
+++ Core/ApplicationGo/hello.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("Hello, Core!")
+	var s, sep string
+
+	for i := 1; i < len(os.Args); i++ {
+
+		s += sep + os.Args[i]
+		sep = " "
+	}
+
+	fmt.Println(s)
 }
