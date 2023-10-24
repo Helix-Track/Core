8583b845331d1cd8d553be14a052aa15b7e80164
Milos Vasic <i@mvasic.ru>
2023-10-24 17:22:21 +0300

    Playground - Example 4.

---
+++ Core/ApplicationGo/Playground/play_4/go.mod
@@ -0,0 +1,3 @@
+module play_4
+
+go 1.20
---
+++ Core/ApplicationGo/Playground/play_4/play.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var word string = "Hello"
+
+	fmt.Println("Word: " + word)
+
+	word = "World"
+
+	fmt.Println("Word: " + word)
+}
