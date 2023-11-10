7bbb48bfe5f97b539542b627b263cc01d37cc374
Milos Vasic <i@mvasic.ru>
2023-11-10 16:44:58 +0300

    Obtaining the data version dinamically.

---
+++ Recipes/defaults.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$DEFAULT_DATA_VERSION" ]; then
+
+    DEFAULT_DATA_VERSION="1.0.13"
+fi
+
+if [ -z "$DEFAULT_DATA_VERSION_ENDPOINT" ]; then
+
+    DEFAULT_DATA_VERSION_ENDPOINT="data_version.txt"
+fi
--- Recipes/installation_parameters_vscode.sh
+++ Recipes/installation_parameters_vscode.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+HERE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DIR_HOME="$(readlink --canonicalize ~)"
 
 if [ -z "$SHARES_SERVER" ]; then
@@ -8,8 +9,45 @@ if [ -z "$SHARES_SERVER" ]; then
     exit 1
 fi
 
+SCRIPT_DEFAULTS="$HERE/defaults.sh"
+
+if ! test -e "$SCRIPT_DEFAULTS"; then
+
+    echo "ERROR: Script not found '$SCRIPT_DEFAULTS'"
+    exit 1
+fi
+
+# shellcheck disable=SC1090
+. "$SCRIPT_DEFAULTS"
+
+if [ -z "$DEFAULT_DATA_VERSION" ]; then
+
+    echo "The 'DEFAULT_DATA_VERSION' is not defined"
+    exit 1
+fi
+
+if [ -z "$DEFAULT_DATA_VERSION_ENDPOINT" ]; then
+
+    echo "The 'DEFAULT_DATA_VERSION_ENDPOINT' is not defined"
+    exit 1
+fi
+
 # shellcheck disable=SC2034
-DATA_VERSION="1.0.14"
+DATA_VERSION="$DEFAULT_DATA_VERSION"
+
+OBTAINED_DATA_VERSION=$(curl "http://$SHARES_SERVER:8081/$DEFAULT_DATA_VERSION_ENDPOINT")
+
+# shellcheck disable=SC2002
+if ! echo "$OBTAINED_DATA_VERSION" | grep "404 Not Found" >/dev/null 2>&1; then
+
+    if ! [ "$DATA_VERSION" == "$OBTAINED_DATA_VERSION" ]; then
+
+        echo "New data version is available: $OBTAINED_DATA_VERSION"
+        echo "Current recipe data version is: $DATA_VERSION"
+
+        DATA_VERSION="$OBTAINED_DATA_VERSION"
+    fi
+fi
 
 # shellcheck disable=SC2034
 DIR_INSTALLATION_HOME="$DIR_HOME/Workspaces/VSCode/Linux/VSCode"
