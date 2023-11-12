00a83253a42d6c9370e89aae49669d7a08c38c0d
Milos Vasic <i@mvasic.ru>
2023-11-12 19:09:56 +0300

    Iconic - Inetgration in progress.

--- Recipes/Iconic/launcher_parameters.sh
+++ Recipes/Iconic/launcher_parameters.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+HERE=$(pwd)
+
+# shellcheck disable=SC2034
+BIN="$HERE/open"
+
+# shellcheck disable=SC2034
+LAUNCHER="$HERE/Assets/Launcher.svg"
+
+
 
 
 
---
+++ Run/Iconic/iconify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$SUBMODULES_HOME" ]; then
+
+  echo "ERROR: The SUBMODULES_HOME is not defined"
+  exit 1
+fi
+
+SCRIPT_ICONIFY="$SUBMODULES_HOME/Iconic/iconify.sh"
+
+if ! test -e "$SCRIPT_ICONIFY"; then
+
+    echo "ERROR: Script not found '$SCRIPT_ICONIFY'"
+    exit 1
+fi
+
+sh "$SCRIPT_ICONIFY"
+
