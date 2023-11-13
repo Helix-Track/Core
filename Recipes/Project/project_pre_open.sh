eda5e3250a3f6dfb02df258025146e49c7dab447
Milos Vasic <i@mvasic.ru>
2023-11-13 16:39:35 +0300

    Paths updated.

--- Recipes/Project/project_pre_open.sh
+++ Recipes/Project/project_pre_open.sh
@@ -2,11 +2,11 @@
 
 HERE=$(pwd)
 
-SCRIPT_CORE_RECIPE_PRE_OPEN="$HERE/Core/Recipes/project_pre_open.sh"
+SCRIPT_CORE_RECIPE_PRE_OPEN="$HERE/Core/Recipes/Project/project_pre_open.sh"
 
 # TODO: Reconnect
 #
-# SCRIPT_PROPRIATERY_RECIPE_PRE_OPEN="$HERE/Propriatery/Recipes/project_pre_open.sh"
+# SCRIPT_PROPRIATERY_RECIPE_PRE_OPEN="$HERE/Propriatery/Recipes/Project/project_pre_open.sh"
 
 EXECUTE_RECIPE() {
 
