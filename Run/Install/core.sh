bc08ce3d8c897a55a274d940b0259bfffd51825d
Milos Vasic <i@mvasic.ru>
2023-11-24 16:51:59 +0300

    "Run configuration paths fixed."

--- Run/Install/core.sh
+++ Run/Install/core.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-HERE="$(pwd)"
-
 if [ -z "$SUBMODULES_HOME" ]; then
 
   echo "ERROR: SUBMODULES_HOME not available"
   exit 1
 fi
 
-cd "$HERE/Core" && sh "$SUBMODULES_HOME/Installable/install.sh"
+sh "$SUBMODULES_HOME/Installable/install.sh"
