40667f8d8b2ef433bc1a1cec4e1150a66a186112
Milos Vasic <i@mvasic.ru>
2023-11-25 17:11:38 +0300

    "Trigger the bot on publish success."

--- Run/Publish/publish_VSCode_Data_Version.sh
+++ Run/Publish/publish_VSCode_Data_Version.sh
@@ -6,12 +6,46 @@ if [ -z "$SUBMODULES_HOME" ]; then
   exit 1
 fi
 
+BOT_SCRIPT="$HERE/Run/Include/Curl/telegram_bot.sh"
+SCRIPT_GET_JQ="$SUBMODULES_HOME/Software-Toolkit/Utils/Sys/Programs/get_jq.sh"
 SCRIPT_PUBLISH="$SUBMODULES_HOME/Software-Toolkit/Utils/VSCode/publish_new_data_version.sh"
 
-if ! test -e "$SCRIPT_PUBLISH"; then
+if ! test -e "$BOT_SCRIPT"; then
 
-    echo "ERROR: Script not found '$SCRIPT_PUBLISH'"
+    echo "ERROR: Script not found '$BOT_SCRIPT'"
     exit 1
 fi
 
-sh "$SCRIPT_PUBLISH" "$1"
+if sh "$SCRIPT_PUBLISH" "$1"; then
+
+  if [ -n "$TELEGRAM_BOT" ] && [ -n "$TELEGRAM_BOT_TOKEN" ] && [ -n "$TELEGRAM_CHAT_ID" ]; then
+
+    if ! test -e "$SCRIPT_PUBLISH"; then
+
+      echo "ERROR: Script not found '$SCRIPT_PUBLISH'"
+      exit 1
+  fi
+
+  if ! test -e "$SCRIPT_GET_JQ"; then
+
+      echo "ERROR: Script not found '$SCRIPT_GET_JQ'"
+      exit 1
+  fi
+
+    if sh "$SCRIPT_GET_JQ" >/dev/null 2>&1; then
+
+        # TODO: Message
+        #
+        MESSAGE="The new version of the VSCode Data has been published"
+
+        ENCODED_MESSAGE=$(jq -rn --arg x "$MESSAGE" '$x|@uri')
+
+        sh "$BOT_SCRIPT" "$TELEGRAM_BOT" "$TELEGRAM_BOT_TOKEN" "$TELEGRAM_CHAT_ID" "$ENCODED_MESSAGE"
+
+    else
+
+        echo "ERROR: JQ not available"
+        exit 1
+    fi
+  fi
+fi
